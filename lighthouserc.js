c4f4dd997f16f50bd69330b025b9024f7104a631
Carina Sa Leitao <carinasaleitao@gmail.com>
2024-04-06 09:03:30 -0300

    att ci.yml and lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,19 +1,7 @@
 module.exports = {
   ci: {
-    collect: {
-      // collect options here
-    },
-    assert: {
-      // assert options here
-    },
     upload: {
-      // upload options here
-    },
-    server: {
-      // server options here
-    },
-    wizard: {
-      // wizard options here
+      target: 'temporary-public-storage',
     },
   },
 };
